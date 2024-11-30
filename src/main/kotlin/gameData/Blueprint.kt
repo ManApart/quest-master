24c659d7ca5f08a11a2172deeed2988f7ff19eb2
ManApart <r@austinkucera.com>
2024-11-30 08:48:58 -0500

    most basic blueprint parsing

--- src/main/kotlin/gameData/Blueprint.kt
+++ src/main/kotlin/gameData/Blueprint.kt
@@ -5,8 +5,41 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Blueprint(
     val Version: String,
-    val Size: Size
+    val Name: String? = null,
+    val Thumbnail: String? = null,
+    val Size: Coordinates,
+    val Entities: List<Entity>,
+    val Tilemaps: TileMap,
 )
 
 @Serializable
-data class Size(val X: Int, val Y: Int)
+data class Coordinates(val X: Float, val Y: Float)
+
+@Serializable
+data class Bounds(val Position: Coordinates, val Size: Coordinates)
+
+@Serializable
+data class Entity(
+    val Entity: String,
+    val Position: Coordinates,
+    val Condition: String? = null,
+    val Group: String? = null,
+    val Attachment: String? = null,
+    val Inversion: String? = null,
+    val Projectile: String? = null,
+    val Rotation: Float? = null,
+    val Speed: String? = null,
+)
+
+@Serializable
+data class TileMap(
+    val Palette: List<String>,
+    val Rooms: List<Room>,
+    val Offset: Coordinates,
+)
+
+@Serializable
+data class Room(
+    val Bounds: Bounds,
+    val Tilemaps: MutableMap<String, String>
+)
